70448a8decfcceef21e4596c35bb65d4bba44747
floh-bhaptics <floh@fahrenbergers.de>
2022-03-30 08:11:49 +0200

    Version bump

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -23,7 +23,7 @@ using MelonLoader;
 // Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
 [assembly: Guid("55e5732f-2617-4feb-b371-52e5dc6e7a7b")]
 
-[assembly: MelonInfo(typeof(Rush_bhaptics.Rush_bhaptics), "Rush_bhaptics", "1.0.0", "Florian Fahrenberger")]
+[assembly: MelonInfo(typeof(Rush_bhaptics.Rush_bhaptics), "Rush_bhaptics", "1.1.0", "Florian Fahrenberger")]
 [assembly: MelonGame("The Binary Mill", "Rush")]
 
 // Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
@@ -36,5 +36,5 @@ using MelonLoader;
 // Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
 // indem Sie "*" wie unten gezeigt eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0")]
+[assembly: AssemblyFileVersion("1.1.0")]
